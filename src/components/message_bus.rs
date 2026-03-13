3bd09537de21948d9b8621702a8922c10ad186ff
Razzor <razzor-codes@proton.me>
2026-03-13 22:22:14 +0200

    test cases for the bus

--- src/components/message_bus.rs
+++ src/components/message_bus.rs
@@ -43,13 +43,107 @@ impl MessageBus {
     }
 
     pub fn route_sync(&mut self) {
+        let mut unprocessed: Vec<Box<dyn Message>> = vec![];
+
         while let Some(message) = self.pending.pop_front() {
-            for consumer in &self.consumers {
-                if consumer.validate(&message) {
-                    consumer.consume(message);
-                    break;
+            // 1. Find the index of the consumer that wants this message.
+            // We only borrow the message here (&message).
+            let consumer_index = self.consumers.iter().position(|c| c.validate(&message));
+
+            match consumer_index {
+                // 2. If we found one, give the message to that specific consumer.
+                Some(idx) => {
+                    self.consumers[idx].consume(message);
+                }
+                // 3. If none found, message is still owned by this scope, so push to unprocessed.
+                None => {
+                    unprocessed.push(message);
                 }
             }
+        }
+
+        self.pending.extend(unprocessed);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockConsumer {
+        accepted_labels: Vec<String>,
+    }
+    impl Consumer for MockConsumer {
+        fn validate(&self, message: &Box<dyn Message>) -> bool {
+            let labels = message.get_labels();
+            for label in labels.iter() {
+                if !self.accepted_labels.contains(&label) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        fn consume(&self, message: Box<dyn Message>) {}
+    }
+
+    #[test]
+    fn bus_creation() {
+        let bus = MessageBus::new();
+
+        assert!(true)
+    }
+
+    #[test]
+    fn sync_routing() {
+        let mut bus = MessageBus::new();
+
+        let mock_consumer = Box::new(MockConsumer {
+            accepted_labels: vec!["label1".to_string(), "label2".to_string()],
+        });
+        bus.add_consumer(mock_consumer);
+
+        let msg_ok1 = Box::new(BaseMessage::new(
+            None,
+            Some(vec!["label1".to_string()]),
+            None,
+        ));
+        let msg_ok2 = Box::new(BaseMessage::new(
+            None,
+            Some(vec!["label1".to_string(), "label2".to_string()]),
+            None,
+        ));
+        let msg_bad = Box::new(BaseMessage::new(
+            None,
+            Some(vec![
+                "label1".to_string(),
+                "label2".to_string(),
+                "label3".to_string(),
+            ]),
+            None,
+        ));
+        bus.add_message(msg_ok1);
+        bus.add_message(msg_ok2);
+        bus.add_message(msg_bad);
+
+        bus.route_sync();
+
+        if bus.done() {
+            assert!(false);
+        }
+
+        let mock_consumer_new = Box::new(MockConsumer {
+            accepted_labels: vec![
+                "label1".to_string(),
+                "label2".to_string(),
+                "label3".to_string(),
+            ],
+        });
+        bus.add_consumer(mock_consumer_new);
+        bus.route_sync();
+
+        if !bus.done() {
+            assert!(false);
         }
     }
 }
